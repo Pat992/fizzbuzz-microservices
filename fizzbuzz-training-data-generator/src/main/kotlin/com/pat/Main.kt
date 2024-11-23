09eda49a3d9de6f15059ef3bcbc32e4a4c769109
Patrick Hettich <patrick_hettich@hotmail.com>
2024-11-23 17:04:15 +0100

    removed unused training data generator

--- fizzbuzz-training-data-generator/src/main/kotlin/com/pat/Main.kt
+++
@@ -1,8 +0,0 @@
-package com.pat
-
-import com.pat.utils.generateFizzBuzzArray
-import com.pat.utils.writeTestDataCsv
-
-const val testDataPathAndName = "./test-data/fizzbuzz.csv"
-
-fun main() = writeTestDataCsv(testDataPathAndName, generateFizzBuzzArray(1000))
